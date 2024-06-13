12269629d7fadb3779d3879298c6fc2ed3f93ca2
Guo Yanpei <73223812+GYPpro@users.noreply.github.com>
2024-06-13 18:36:30 +0800

    add class RB_Tree\Node

--- Dev/08/RB_Tree.h
+++ Dev/08/RB_Tree.h
@@ -19,19 +19,43 @@ namespace myDS
 
         protected:
             class Node{
+            public:
                 VALUE value;
                 COLOR color;
                 Node *leftSubTree,  //左子树根节点指针
                      *rightSubTree, //右子树根节点指针
                      *parent;       //父亲节点指针
                 
-                constexpr Node() : 
+                constexpr Node() : //构造函数
                     value(VALUE()), 
                     color(COLOR::RED),
                     leftSubTree(nullptr),
                     rightSubTree(nullptr),
                     parent(nullptr) { }; 
-            }
+
+                //获取祖父节点指针
+                inline Node * getGrandParent() { 
+                    if(parent == nullptr) return nullptr;
+                    else return parent->parent;
+                }
+
+                //获取叔叔节点指针
+                inline Node * getUncle() {
+                    Node* __gp = this->getGrandParent();
+                    if(__gp == nullptr) return nullptr;
+                    else if(parent == __gp->rightSubTree) return __gp->leftSubTree;
+                    else return __gp->rightSubTree;
+                }
+
+                //获取兄弟节点指针
+                inline Node * getSibling(){
+                    if(parent == nullptr) return nullptr;
+                    else if(parent->leftSubTree == this) return parent->rightSubTree;
+                    else return parent->leftSubTree;
+                }
+
+            };
+        
     };
 } // namespace myDS
 #endif
