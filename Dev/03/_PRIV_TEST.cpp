739149f078632c8a8c08a29220ab745f5e445559
Guo Yanpei <73223812+GYPpro@users.noreply.github.com>
2024-06-23 14:21:54 +0800

    build up dataBlock

---
+++ Dev/03/_PRIV_TEST.cpp
@@ -0,0 +1,203 @@
+#define DS_TOBE_TEST dataBlock
+
+#define _PRIVATE_DEBUG
+#define __DETIL_DEBUG_OUTPUT
+
+#include "Dev\03\dataBlock.hpp"
+
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+using TBT = myDS::dataBlock<int>;
+
+void accuracyTest() {//结构正确性测试
+
+    TBT tc = TBT();
+    for(;;)
+    {
+        string op;
+        cin >> op;
+        if(op == "clr") { //清空
+            tc.clear();
+        } else if(op == "q") //退出测试
+        {
+            return;
+        } else if(op == "pb")//push_back
+        {
+            int c;
+            cin >> c;
+            tc.push_back(c);
+        // } else if(op == "pf")//push_frount
+        // {
+        //     int c;
+        //     cin >> c;
+        //     tc.push_frount(c);
+        } else if(op == "at")//随机访问
+        {
+            int p;
+            cin >> p;
+            cout << tc[p] << "\n";
+        // } else if(op == "delEL")//删除所有等于某值元素
+        // {
+        //     int p;
+        //     cin >> p;
+        //     cout << tc.erase(p) << "\n";
+        // } else if(op == "delPS")//删除某位置上的元素
+        // {
+        //     int p;
+        //     cin >> p;
+        //     cout << tc.erase(tc.get(p)) << "\n";
+        } else if(op == "iterF") //正序遍历
+        {
+            tc.innerPrint();
+            cout << "Iter with index:\n";
+            for(int i = 0;i < tc.size();i ++) cout << tc[i] << " ";cout << "\n";
+            cout << "Iter with begin end\n";
+            for(auto x = tc.begin();x != tc.end();x ++) cout << (*x) << " ";cout << "\n";
+            cout << "Iter with AUTO&&\n";
+            for(auto x:tc) cout << x << " ";cout << "\n";  
+        } else if(op == "iterB") //倒序遍历
+        {
+            tc.innerPrint();
+            cout << "Iter with index:\n";
+            for(int i = 0;i < tc.size();i ++) cout << tc[tc.size()-1-i] << " ";cout << "\n";
+            cout << "Iter with begin end\n";
+            for(auto x = tc.rbegin();x != tc.rend();x ++) cout << (*x) << " ";cout << "\n";
+            // cout << "Iter with AUTO&&\n";."\n";  
+        } else if(op == "mv")//单点修改
+        {
+            int p;
+            cin >> p;
+            int tr;
+            cin >> tr;
+            tc[p] = tr;
+        } else if(op == "")
+        {
+            
+        } else {
+            op.clear();
+        }
+    }
+}
+
+
+/*
+  pb 1
+  pb 2
+  pb 3
+  pb 4
+  iterF
+--Header[0x662720]: 0
+--Tail[0x662770]: 0
+-----------
+cur:6
+[0x662720] ->next:0x662540 ->priv:0 ||data:0
+[0x662540] ->next:0x662590 ->priv:0x662720 ||data:3
+[0x662590] ->next:0x6625e0 ->priv:0x662540 ||data:1
+[0x6625e0] ->next:0x662630 ->priv:0x662590 ||data:2
+[0x662630] ->next:0x662680 ->priv:0x6625e0 ||data:3
+[0x662680] ->next:0x6626d0 ->priv:0x662630 ||data:4
+[0x6626d0] ->next:0x662770 ->priv:0x662680 ||data:3
+[0x662770] ->next:0 ->priv:0x6626d0 ||data:0
+Iter with index:
+3 1 2 3 4 3
+Iter with begin end
+3 1 2 3 4 3
+Iter with AUTO&&
+3 1 2 3 4 3
+  iterB
+--Header[0x662720]: 0
+--Tail[0x662770]: 0
+-----------
+cur:6
+[0x662720] ->next:0x662540 ->priv:0 ||data:0
+[0x662540] ->next:0x662590 ->priv:0x662720 ||data:3
+[0x662590] ->next:0x6625e0 ->priv:0x662540 ||data:1
+[0x6625e0] ->next:0x662630 ->priv:0x662590 ||data:2
+[0x662630] ->next:0x662680 ->priv:0x6625e0 ||data:3
+[0x662680] ->next:0x6626d0 ->priv:0x662630 ||data:4
+[0x6626d0] ->next:0x662770 ->priv:0x662680 ||data:3
+[0x662770] ->next:0 ->priv:0x6626d0 ||data:0
+Iter with index:
+3 4 3 2 1 3
+Iter with begin end
+3 4 3 2 1 3
+  delEL 3
+3
+  iterF
+--Header[0x662720]: 0
+--Tail[0x662770]: 0
+-----------
+cur:3
+[0x662720] ->next:0x662590 ->priv:0 ||data:0
+[0x662590] ->next:0x6625e0 ->priv:0x662720 ||data:1
+[0x6625e0] ->next:0x662680 ->priv:0x662590 ||data:2
+[0x662680] ->next:0x662770 ->priv:0x6625e0 ||data:4
+[0x662770] ->next:0 ->priv:0x662680 ||data:0
+Iter with index:
+1 2 4
+Iter with begin end
+1 2 4
+Iter with AUTO&&
+1 2 4
+  delPS 1
+1
+  clr
+Unexpected Delete at :4 with next:16187728
+  pb 1
+  pb 2
+  iterF
+--Header[0x6625e0]: 0
+--Tail[0x662680]: 0
+-----------
+cur:2
+[0x6625e0] ->next:0x662540 ->priv:0 ||data:0
+[0x662540] ->next:0x662630 ->priv:0x6625e0 ||data:1
+[0x662630] ->next:0x662680 ->priv:0x662540 ||data:2
+[0x662680] ->next:0 ->priv:0x662630 ||data:0
+Iter with index:
+1 2
+Iter with begin end
+1 2
+Iter with AUTO&&
+1 2
+  delPS 0
+1
+  delEL 2
+1
+  iterF
+--Header[0x6625e0]: 0
+--Tail[0x662680]: 0
+-----------
+cur:0
+[0x6625e0] ->next:0x662680 ->priv:0 ||data:0
+[0x662680] ->next:0 ->priv:0x6625e0 ||data:0
+Iter with index:
+
+Iter with begin end
+
+Iter with AUTO&&
+
+
+ 
+ */
+
+void memLeakTest() {//内存泄漏测试
+    TBT tc = TBT();
+    for(;;){
+        tc.push_back(1);
+        tc.push_back(1);
+        tc.push_back(1);
+        tc.push_back(1);
+        tc.clear();
+    }
+}
+
+signed main()
+{
+    accuracyTest();
+    // memLeakTest();
+}
---
+++ Dev/03/dataBlock.hpp
@@ -0,0 +1,201 @@
+// #define _PRIVATE_DEBUG
+#ifndef DATA_BLOCK_HPP
+#define DATA_BLOCK_HPP
+
+#include <vector>
+#include <map>
+
+#define _PRIVATE_DEBUG
+
+#ifdef _PRIVATE_DEBUG
+#include <iostream>
+#endif
+
+namespace myDS
+{
+    template<typename VALUE_TYPE>
+    class dataBlock{
+    protected: 
+
+    private:
+        class _iterator 
+        {
+        private:
+            VALUE_TYPE *_ptr;
+            std::pair<std::size_t,std::size_t> loc;
+            dataBlock<VALUE_TYPE> * _upper_pointer;
+
+        public:
+            enum __iter_dest_type
+            {
+                front,
+                back
+            };
+            __iter_dest_type _iter_dest;
+            
+            _iterator(myDS::dataBlock<VALUE_TYPE> *_upper,std::pair<std::size_t,std::size_t> _loc,__iter_dest_type _d)
+            {
+                _upper_pointer = _upper;
+                loc = _loc;
+                _ptr = &_upper_pointer->_indexs[loc.first][loc.second];
+                _iter_dest = _d;
+            }
+
+            VALUE_TYPE & operator*()
+            {
+                return (*_ptr);
+            }
+
+            VALUE_TYPE *operator->()
+            {
+                return _ptr;
+            }
+
+            myDS::dataBlock<VALUE_TYPE>::_iterator operator++() {
+                if(_iter_dest == front)
+                {
+                    loc = _upper_pointer->nextPII(loc);
+                }
+                else
+                {
+                    loc = _upper_pointer->prevPII(loc);
+                }
+                _ptr = &_upper_pointer->_indexs[loc.first][loc.second];
+                return myDS::dataBlock<VALUE_TYPE>::_iterator(_upper_pointer,loc,_iter_dest);
+            }
+
+            myDS::dataBlock<VALUE_TYPE>::_iterator operator++(int) {
+                myDS::dataBlock<VALUE_TYPE>::_iterator old = *this;
+                if(_iter_dest == front)
+                {
+                    loc = _upper_pointer->nextPII(loc);
+                }
+                else
+                {
+                    loc = _upper_pointer->prevPII(loc);
+                }
+                _ptr = &_upper_pointer->_indexs[loc.first][loc.second];
+                return old;
+            }
+
+            bool operator==(myDS::dataBlock<VALUE_TYPE>::_iterator _b) {
+                return _ptr == _b._ptr;
+            }
+
+            bool operator!=(myDS::dataBlock<VALUE_TYPE>::_iterator _b) {
+                return _ptr != _b._ptr;
+            }
+        };
+
+        std::vector<VALUE_TYPE *> _indexs;
+        std::pair<std::size_t,std::size_t> _cap = {0,0};
+        std::size_t consMEX = 1;
+        std::size_t _size = 0;
+
+
+        void _expension()
+        {
+            VALUE_TYPE *temp = new VALUE_TYPE[consMEX];
+            _indexs.push_back(temp);
+            consMEX *= 2;
+            _cap.first++;
+            _cap.second = 0;
+        }
+
+        std::size_t getMEX(std::int32_t p)
+        {
+            if(p <= 0) return p+1; 
+            return (1 << (p-1));
+        }
+
+        std::pair<std::size_t,std::size_t> nextPII(std::pair<std::size_t,std::size_t> p)
+        {
+            p.second++;
+            if(p.second >= getMEX(p.first))
+            {
+                p.first++;
+                p.second = 0;
+            }
+            return p;
+        }  
+
+        std::pair<std::size_t,std::size_t> prevPII(std::pair<std::size_t,std::size_t> p)
+        {
+            p.second --;
+            if(p.second < 0)
+            {
+                p.first--;
+                p.second = getMEX(p.first) - 1;
+            }
+            return p;
+        }
+
+    public:
+        dataBlock(){
+            _indexs.push_back(new VALUE_TYPE[1]);
+        }
+
+        ~dataBlock(){
+            clear();
+            delete [] (_indexs[0]);
+        }
+
+        void push_back(VALUE_TYPE t) {
+            if(_cap.second >= consMEX) {
+                _expension();
+            }
+            _indexs[_cap.first][_cap.second] = t;
+            _cap.second++;
+            _size++;
+        }
+
+        void clear() {
+            for(auto x:_indexs) delete [] x;
+            _indexs.push_back(new VALUE_TYPE());
+        }
+
+        std::size_t size() { }
+
+        myDS::dataBlock<VALUE_TYPE>::_iterator begin() {
+            return myDS::dataBlock<VALUE_TYPE>::_iterator(this,{0,0},myDS::dataBlock<VALUE_TYPE>::_iterator::front);
+        }
+        
+        myDS::dataBlock<VALUE_TYPE>::_iterator rbegin() {
+            return myDS::dataBlock<VALUE_TYPE>::_iterator(this,{_cap.first,_cap.second},myDS::dataBlock<VALUE_TYPE>::_iterator::back);
+        }
+
+        myDS::dataBlock<VALUE_TYPE>::_iterator end() {
+            return myDS::dataBlock<VALUE_TYPE>::_iterator(this,nextPII(_cap),myDS::dataBlock<VALUE_TYPE>::_iterator::front);
+        }
+
+        myDS::dataBlock<VALUE_TYPE>::_iterator rend() {
+            return myDS::dataBlock<VALUE_TYPE>::_iterator(this,prevPII({0,0}),myDS::dataBlock<VALUE_TYPE>::_iterator::back);
+        }
+
+#ifdef _PRIVATE_DEBUG
+        void innerPrint() {
+            std::pair<std::size_t,std::size_t> p = {0,0};
+            while(p != _cap) {
+                if(p.second == 0) std::cout << "\nBlock : [" << p.first << "] at:" << _indexs[p.first] << "\n";
+                std::cout << _indexs[p.first][p.second] << " ";
+                p = nextPII(p);
+            }
+        }
+#endif
+
+        VALUE_TYPE & operator[](std::size_t p) {
+            std::int32_t onord = 0;
+            std::size_t tmp = p;
+            while(tmp) {
+                tmp >>= 1;
+                onord++;
+            }
+#ifdef __DETIL_DEBUG_OUTPUT
+            std::cout << "onord:" << onord << " p:" << p << " GETMEX :"<< getMEX(onord-1) << " index:{" << onord << "," << p - getMEX(onord-1) << "}\n";
+#endif
+            return _indexs[onord][p - getMEX(onord-1)];
+
+        }
+    };
+}
+#endif
